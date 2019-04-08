fdb4d84a3f95dcc1d1cdd5240f25ad99506b3995
Diego Herrera <goherrerago@gmail.com>
2019-04-08 09:13:55 -0500

    Actualizando

--- pages/modulo_administrar/administrar.php
+++ pages/modulo_administrar/administrar.php
@@ -3,7 +3,6 @@
  ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -193,7 +192,5 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="../../dist/js/sb-admin-2.js"></script>
-
 </body>
-
 </html>
