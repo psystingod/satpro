ff800c3f134fc82160c23afe11d70eab99fb03ba
Diego Herrera <goherrerago@gmail.com>
2019-04-10 17:39:45 -0600

    actualizando

--- pages/Prueba.php
+++ pages/Prueba.php
@@ -1,1 +1,15 @@
-
+<?php
+$n = "004800";
+$len = strlen($n);
+$concatenate = "";
+for ($i=0; $i < $len; $i++) {
+    if ($n[$i] == "0") {
+        $add = $n[$i];
+        $concatenate .= $add;
+    }else {
+        break;
+    }
+}
+//$n = intval($n);
+echo var_dump($concatenate);
+?>
