d987b033d3326e8b6bf3a05962d77b77239e5e6e
Diego Herrera <goherrerago@gmail.com>
2019-12-04 10:12:15 -0600

    Contratos terminados

--- pages/modulo_cxc/cxc.php
+++ pages/modulo_cxc/cxc.php
@@ -119,6 +119,8 @@
                                 </li>
                                 <li><a href="ventasManuales.php">Ventas manuales</a>
                                 </li>
+                                <li><a href="imprimirFacturas.php" data-toggle="modal" data-target="#imprimirFacturas">Imprimir facturas</a>
+                                </li>
                             </ul>';
                         }elseif ($_SESSION["rol"] == 'informatica' || $_SESSION["rol"] == 'atención al cliente' || $_SESSION["rol"] == 'jefatura') {
                             echo
@@ -128,6 +130,8 @@
                                 <li><a href="gestionCobros.php">Gestion de cobros</a>
                                 </li>
                                 <li><a href="ventasManuales.php">Ventas manuales</a>
+                                </li>
+                                <li><a href="imprimirFacturas.php">Imprimir facturas</a>
                                 </li>
                             </ul>';
                         }
@@ -442,6 +446,43 @@
                 </div>
               </div>
           </div><!-- Fin Modal ABONOS -->
+
+          <!-- Modal Consultar/Eliminar abonos -->
+          <div id="imprimirFacturas" class="modal fade" role="dialog">
+            <div class="modal-dialog modal-lg">
+
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div style="background-color: #00897B; color:white;" class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Imprimir facturas</h4>
+                </div>
+                <form id="generarFacturas" action="php/generarFacturas.php?" method="POST">
+                <div class="modal-body">
+                  <div class="row">
+                      <div class="col-md-12">
+                          <input class="form-control" type="text" id="caja_busqueda" name="caja_busqueda" value="" placeholder="Número de recibo, Código del cliente, Fecha del abono, Código del cobrador">
+                          <br>
+                          <div class="" id="datos">
+
+                          </div>
+                      </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="submit" class="btn btn-primary btn-lg btn-block" name="submit" value="Generar Facturas">
+                        </div>
+                        <div class="col-md-6">
+                            <button type="button" class="btn btn-default btn-lg btn-block" data-dismiss="modal">Cancelar</button>
+                        </div>
+                    </div>
+                </form>
+                </div>
+              </div>
+            </div>
+        </div><!-- Fin Modal ABONOS -->
         </div>
         <!-- /#page-wrapper -->
 
